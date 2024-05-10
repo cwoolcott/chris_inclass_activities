268b77b4e3eedac833bda04ac57e394f49861a32
cwoolcott <christopher.woolcott@gmail.com>
2024-05-09 22:25:45 -0500

    middleware and more

--- 11-Express/02-cars/server.js
+++ 11-Express/02-cars/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const fs = require("fs");
-const carData = require("./data/cars.json")
+//const carData = require("./data/cars.json")
 const app = express();
+const api = require("./routes");
+
+const logging = require('./middleware/logging')
 
 app.use(express.static("public"));
 
@@ -9,48 +12,20 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-const PORT = 3001;
+app.use(logging);
+
+const PORT = process.env.PORT || 3001;
 
 //html routes
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + "/public/cars.html")
 });
 
-app.get('/addcar', (req, res)=>{
+app.get('/addcar',  (req, res)=>{
     res.sendFile(__dirname + "/public/addcar.html")
 });
 
-
-//api routes
-app.get('/api/cars', (req, res)=>{
-    res.json(carData)
-});
-
-app.post('/api/cars', (req, res)=>{
-   
-    
-    const newCarObj = {
-        id: req.body.carId,
-        make: req.body.make,
-        model: req.body.model,
-        year: req.body.year,
-        sold: false
-    }
-    console.log(newCarObj);
-    carData.push(newCarObj);
-    res.json(`The ${req.body.make} was added!`);
-    //console.log(`${req.method} was used!`)
-});
-
-app.put('/api/cars', (req, res)=>{
-    res.json(`${req.method} was used!`);
-    console.log(`${req.method} was used!`)
-});
-
-app.delete('/api/cars', (req, res)=>{
-    res.json(`${req.method} was used and everything was deleted!`);
-    console.log(`${req.method} was used and everything was deleted!`);
-});
+app.use('/api', api);
 
 app.listen(PORT, () => {
     console.log(`Listening on Port: ${PORT}`);
