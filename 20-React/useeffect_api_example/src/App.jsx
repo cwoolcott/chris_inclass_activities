315567d0a9f1a383dae19cef3cdfd09b17692e3b
Chris Woolcott <christopher.woolcott@gmail.com>
2024-07-23 10:54:40 -0500

    useeffect api example

---
+++ 20-React/useeffect_api_example/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import SearchGiphy from './components/searchgiphy';
+
+const App = () => {
+  return (
+    <div className='App' style={{ backgroundColor: 'teal' }}>
+      {<SearchGiphy />}
+    </div>
+  );
+};
+
+export default App;
